748201e788f650a5dad28407136c228b7ff640b3
Sachin Gupta <sachin070gupta@gmail.com>
2021-03-17 22:37:59 +0530

    Update TruckService.java

--- com/cg/service/TruckService.java
+++ com/cg/service/TruckService.java
@@ -1,9 +1,25 @@
 package com.cg.service;
 
+import com.cg.bean.Truck;
+
 public interface TruckService {
 
 	void showTruck();
 
-	void bookTruck(String custId);
+	boolean isCustIdValid(String custId);
+
+	boolean isValidTruckId(int truckId);
+
+	boolean isTruckAvailable(int truckId,int truckWant);
+
+	boolean isValidMobile(String mobile);
+
+	boolean isValidDate(String date);
+
+	int createBooking(Truck tr, int truckId, int truckWant, String custId, String mobile, String dateStr);
+
+	Truck getTruckObj(int truckId);
+
+	void showBookingDetails();
 
 }
