e78239edf7882a659fd3c282c680f21447c885e6
Sachin Gupta <sachin070gupta@gmail.com>
2021-03-17 22:37:13 +0530

    Changed UI
    Added all the sysout statement in UI Main, rather than in DAO layer.
    Also, all the scanner work is also replaced to UI

--- com/cg/ui/TruckMain.java
+++ com/cg/ui/TruckMain.java
@@ -3,6 +3,7 @@ package com.cg.ui;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import com.cg.bean.Truck;
 import com.cg.service.TruckService;
 import com.cg.service.TruckServiceImpl;
 
@@ -20,17 +21,53 @@ public class TruckMain {
 			int choice = getChoice(scan);
 			switch (choice) {
 			case 1:
-				String idRegex = "^([A-Z]\\d{6})$";
-				String custId;
-				System.out.println("Book Trucks");
-				System.out.println("Enter customer ID : ");
-				custId = scan.next();
-				if (custId.matches(idRegex)) {
-					tService.showTruck();
-					tService.bookTruck(custId);
-				} else
-					System.out.println("Wrong Id Format");
+				try {
+					String custId;
+					int truckId;
+					String mobile;
+					String date;
+					Truck tr;
+					System.out.println("Book Trucks");
+					System.out.println("Enter customer ID : ");
+					custId = scan.next();
+					if (tService.isCustIdValid(custId)) {
+						System.out.println("Please see the truck details below ");
+						System.out.println("TruckId    TruckType         Origin     Destination     Charge    AvailableNos");
+						tService.showTruck();
+						System.out.println("Please Enter the Truck ID : ");
+						truckId = scan.nextInt();
+						if(tService.isValidTruckId(truckId)) {
+							tr = tService.getTruckObj(truckId);
+							System.out.println("Enter the no. of Trucks : ");
+							int truckWant = scan.nextInt();
+							if(tService.isTruckAvailable(truckId,truckWant)) {
+								System.out.println("Enter customer mobile : ");
+								mobile = scan.next();
+								if(tService.isValidMobile(mobile)) {
+									System.out.println("Enter Date of Transportation (YYYY-MM-DD): ");
+									date = scan.next();
+									if(tService.isValidDate(date)) {
+										System.out.println("Thank you. Your booking Id is ."+tService.createBooking(tr,truckId,truckWant,custId,mobile,date));
+									}else
+										System.out.println("Date is not correct.");
+								}else
+									System.out.println("Mobileno. should be of 10 digit. ");
+							}else
+								System.out.println("Given number of trucks is/are not available.");
+						}else
+							System.out.println("Given Truck Id doesn't Exist.");
+					} else
+						System.out.println("Wrong Id Format");
+
+				}catch(InputMismatchException e) {
+					System.out.println("Wrong Input !");
+				}
 				break;
+//			case 2:
+//				System.out.println("List of Bookings :- ");
+//				System.out.println("BookingId    CustId         TruckType     Mobile     DateOfBooking    TotalCost");
+//				tService.showBookingDetails();
+//				break;
 			case 2:
 				System.out.println("Exiting System, Thank You for Using ");
 				System.exit(0);
@@ -49,7 +86,10 @@ public class TruckMain {
 				System.out.println("** WELCOME to TRUCK BOOKING SYSTEM **");
 				System.out.println("Enter Your Choice : ");
 				System.out.println("1. Book Trucks");
+//				System.out.println("2. Show Truck Details");
+//				System.out.println("2. All Booking Details");
 				System.out.println("2. Exit");
+				System.out.println("-------------------------------------");
 				choice = scan.nextInt();
 			} catch (InputMismatchException e) {
 				System.out.println("Enter Correct Choice !");
